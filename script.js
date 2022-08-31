6791c0a7af0633eac808e72df20fd85adeaa85ad
the00sheep <schepp.andreas@gmail.com>
2022-08-31 11:43:59 +0300

    Added oop and changing speed slider

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
-let gamespeed = 15;
+let gameSpeed = 10;
 
 const backLayer1 = new Image();
 backLayer1.src = './bg/layer-1.png';
@@ -15,17 +15,64 @@ backLayer4.src = './bg/layer-4.png';
 const backLayer5 = new Image();
 backLayer5.src = './bg/layer-5.png';
 
-let x = 0;
-let x2 = 2400;
+const slider = document.getElementById('slider');
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById('showGameSpeed');
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener('change', function(e){
+    gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = e.target.value;
+});
+
+class Layer {
+
+  constructor(image, speedModifier){
+    this.x = 0;
+    this.y = 0;
+    this.width = 2400;
+    this.height = 700;
+    //this.x2 = this.width;
+    this.image = image;
+    this.speedModifier = speedModifier;
+    this.speed = gameSpeed * this.speedModifier;
+  }
+
+  update(){
+    this.speed = gameSpeed * this.speedModifier;
+
+    if(this.x <= -this.width){
+      this.x =  0;
+    }
+    /*if(this.x2 <= -this.width){
+      this.x2 =  this.width + this.x - this.speed;
+    }*/
+    this.x = Math.floor(this.x - this.speed);
+    //this.x2 = Math.floor(this.x2 - this.speed);
+  }
+
+
+  draw(){
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+  }
+
+}
+
+const layer1  = new Layer(backLayer1, 0.2);
+const layer2  = new Layer(backLayer2, 0.4);
+const layer3  = new Layer(backLayer3, 0.6);
+const layer4  = new Layer(backLayer4, 0.8);
+const layer5  = new Layer(backLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
 
 function animate(){
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  ctx.drawImage(backLayer4, x, 0);
-  ctx.drawImage(backLayer4, x2, 0);
-  if (x < -2400) x = 2400 + x2 - gamespeed;
-  else x -=gamespeed;
-  if (x2 < -2400) x2 = 2400 + x - gamespeed;
-  else x2-= gamespeed;
+  gameObjects.forEach(object => {
+      object.update();
+      object.draw();
+  });
+
   requestAnimationFrame(animate);
 }
 animate();
